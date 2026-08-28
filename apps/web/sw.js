00073fea8ba6b6011962010c95d624f3dfd4f1f8
fikdaf <fikrizulfikar09@gmail.com>
2026-08-28 07:16:30 +0700

    feat(web): add offline service worker

---
+++ apps/web/sw.js
@@ -0,0 +1,29 @@
+const CACHE = "jayantara-web-v1";
+const APP_SHELL = ["./", "./index.html", "./manifest.webmanifest", "./icons/icon.svg"];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(caches.open(CACHE).then((cache) => cache.addAll(APP_SHELL)).then(() => self.skipWaiting()));
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) => Promise.all(keys.filter((key) => key !== CACHE).map((key) => caches.delete(key)))).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  if (event.request.method !== "GET") return;
+  const request = event.request;
+  event.respondWith(
+    caches.match(request).then((cached) => {
+      if (cached) return cached;
+      return fetch(request).then((response) => {
+        if (response.ok && new URL(request.url).origin === self.location.origin) {
+          const copy = response.clone();
+          caches.open(CACHE).then((cache) => cache.put(request, copy));
+        }
+        return response;
+      });
+    })
+  );
+});
